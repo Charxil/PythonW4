239ead5e80b5226d793d4347e7ecc2f4078819fc
Aye Chan Myint <9.susan2@gmail.com>
2025-02-15 23:31:25 +0800

    Add 1_year

---
+++ 1_year.py
@@ -0,0 +1,11 @@
+day = 365
+hour = 24
+hourinyear= day * hour
+print(f'There are {hourinyear} hours in a year')
+minute = 60 * hour ## for each day
+minindecade = minute * day * 10
+print(f'There are {minindecade} minutes in a decade')
+second = 60 * minute ## for each day
+age = 28
+secondinage = second * day * age
+print(f'I am {age} years old, so I have lived for {secondinage} seconds')
